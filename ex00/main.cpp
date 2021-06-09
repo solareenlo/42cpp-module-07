22273e1cc5be517891bd17ab22847422ea4ea4a1
solareenlo <solareenlo@gmail.com>
2021-06-10 07:18:50 +0900

    fix: ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 07:00:42 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/06/10 07:14:34 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/06/10 07:18:25 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -14,46 +14,18 @@
 #include <iomanip>
 #include "whatever.hpp"
 
-int main() {
-    {
-        char a = 65, b = 66;
-        std::cout << a << " " << b << std::endl;
-        ::swap(a, b);
-        std::cout << a << " " << b << std::endl;
-        std::cout << max(a, b) << std::endl;
-        std::cout << min(a, b) << std::endl;
-        std::cout << std::endl;
-    }
-    {
-        int a = 1, b = 2;
-        std::cout << a << " " << b << std::endl;
-        ::swap(a, b);
-        std::cout << a << " " << b << std::endl;
-        std::cout << max(a, b) << std::endl;
-        std::cout << min(a, b) << std::endl;
-        std::cout << std::endl;
-    }
-    {
-        float a = 1.0, b = 2.0;
-        std::cout << std::fixed << std::setprecision(1);
-        std::cout << a << " " << b << std::endl;
-        ::swap(a, b);
-        std::cout << a << " " << b << std::endl;
-        std::cout << max(a, b) << std::endl;
-        std::cout << min(a, b) << std::endl;
-        std::cout << std::defaultfloat;
-        std::cout << std::endl;
-    }
-    {
-        double a = 1.0, b = 2.0;
-        std::cout << std::fixed << std::setprecision(1);
-        std::cout << a << " " << b << std::endl;
-        ::swap(a, b);
-        std::cout << a << " " << b << std::endl;
-        std::cout << max(a, b) << std::endl;
-        std::cout << min(a, b) << std::endl;
-        std::cout << std::defaultfloat;
-        std::cout << std::endl;
-    }
+int main(void) {
+    int a = 2;
+    int b = 3;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+    std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
     return 0;
 }
