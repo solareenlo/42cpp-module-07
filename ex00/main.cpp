9e1856fe87aecca53cc923415ca0ea6b7cb611f6
solareenlo <solareenlo@gmail.com>
2021-06-10 17:51:33 +0900

    fix

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 07:00:42 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/06/10 07:18:25 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/06/10 17:51:18 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@
 int main(void) {
     int a = 2;
     int b = 3;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
     ::swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
     std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
     std::string c = "chaine1";
     std::string d = "chaine2";
+    std::cout << "c = " << c << ", d = " << d << std::endl;
     ::swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
